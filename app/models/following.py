ce57a1b63fa28fcccc9cadec2073e4d1da987ab9
Dillon <dft609@gmail.com>
2021-03-03 12:16:29 -0700

    work on many to many relationships

---
+++ app/models/following.py
@@ -0,0 +1,17 @@
+from .db import db
+
+class Following(db.Model):
+  __tabelname__: "Followings"
+
+  id = db.Column(db.Integer, primary_key=True)
+  follower_id = db.Column(db.Integer, ForeignKey("users.id"))
+  followee_id = db.Column(db.Integer, ForeignKey("users.id"))
+
+  user = db.relationship("User")
+
+  def to_dict():
+    return {
+      "id": self.id,
+      "follower_id": self.follower_id,
+      "followee_id": self.followee_id
+    }

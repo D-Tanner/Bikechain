dd9bd02d3833c8e95b4ccdb4cee1c8f114460293
Dillon <dft609@gmail.com>
2021-03-03 17:29:28 -0700

    seeder data

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -1,6 +1,10 @@
 from werkzeug.security import generate_password_hash
 from app.models import db
 from app.models.user_and_ride import User
+from sqlalchemy.sql import table, column
+from sqlalchemy import String, Integer
+from alembic import op
+
 
 # Adds a demo user, you can add other users here if you want
 def seed_users():
@@ -24,8 +28,32 @@ def seed_users():
 
     db.session.add_all([one, two, three, four])
 
-    db.session.commit()
 
+    users_committed_rides = table('users_committed_rides',
+        column('userId', Integer),
+        column('rideId', Integer),
+    )
+
+    following = table('followings',
+        column('user_id', Integer),
+        column('following_user_id', Integer),
+    )
+
+    op.bulk_insert(users_committed_rides,
+    [
+        {"userId": 1, "rideId": 1},
+        {"userId": 1, "rideId": 2},
+        {"userId": 2, "rideId": 3},
+        {"userId": 2, "rideId": 4},
+        {"userId": 3, "rideId": 2},
+        {"userId": 3, "rideId": 4},
+        {"userId": 4, "rideId": 1},
+        {"userId": 4, "rideId": 2},
+        {"userId": 4, "rideId": 3},
+        {"userId": 4, "rideId": 4},
+    ])
+
+    db.session.commit()
 # Uses a raw SQL query to TRUNCATE the users table.
 # SQLAlchemy doesn't have a built in function to do this
 # TRUNCATE Removes all the data from the table, and resets

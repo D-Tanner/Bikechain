9848e2491be4be472d25f01652377adbbc3dd26d
Dillon <dft609@gmail.com>
2021-03-06 16:17:43 -0700

    routing

--- react-app/src/components/CreateRide/CreateRide.js
+++ react-app/src/components/CreateRide/CreateRide.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom'
+import { createNewRide } from '../../services/rides';
 import "./CreateRide.css"
 
 
-const CreateRide = () => {
+const CreateRide = ({ user }) => {
 
   const history = useHistory();
   const [title, setTitle] = useState("");
@@ -13,10 +14,23 @@ const CreateRide = () => {
   const [lat, setLat] = useState();
   const [long, setLong] = useState();
   const [isLocal, setIsLocal] = useState(false);
-  const [level, setIsLevel] = useState("");
+  const [level, setLevel] = useState("");
+  const [errors, setErrors] = useState([])
+
+  const postRide = async (e) => {
+    e.preventDefault()
+    const newRide = await createNewRide(user.id, title, content, startTime, endTime, lat, long, isLocal, level)
+    if (newRide.errors) {
+      setErrors(newRide.errors)
+    } else {
+      history.push("/")
+    }
+  }
 
   return (
-    <div className="container">CreateRide</div>
+    <div className="container">
+      Create Ride
+    </div>
   )
 }
 

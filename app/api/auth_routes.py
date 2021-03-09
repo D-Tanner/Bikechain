cd416351a02083d94e8f774d7f20d25712f23768
Dillon <dft609@gmail.com>
2021-03-09 12:31:47 -0700

    user signup

--- app/api/auth_routes.py
+++ app/api/auth_routes.py
@@ -80,7 +80,10 @@ def sign_up():
         db.session.add(user)
         db.session.commit()
         login_user(user)
-        return user.to_dict()
+        following_dict = [following.to_dict() for following in user.followers]
+
+        return {"user": user.to_dict(),
+                "following": following_dict}
     return {'errors': validation_errors_to_error_messages(form.errors)}
 
 

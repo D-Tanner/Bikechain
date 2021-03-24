269252cb015bc21d0d071ecbeedd69516763fe67
Dillon <dft609@gmail.com>
2021-03-24 16:01:45 -0600

    image

--- react-app/src/components/ProfilePage/ProfilePage.js
+++ react-app/src/components/ProfilePage/ProfilePage.js
@@ -174,12 +174,16 @@ const ProfilePage = () => {
                     }}
                   >
                     <div className="following-grid-container">
-                      {/* <div className="profile-image">
 
-                      </div> */}
                       <div className="user-level">
                         <div className="level-image-feed">
                           <img id="level-image-feed" src={getLevel(user.level)} alt=""></img>
+                        </div>
+                      </div>
+                      <div className="profile-image-container">
+                        <div className="profile-image-in-feed">
+
+                          {user.profileImage && <img id="profile-image-in-feed" src={user.profileImage} alt=""></img>}
                         </div>
                       </div>
                       <div className="user-username">{user.username}</div>
